1e999b4b0531b027a1c0742689c8be1ef4d3b7db
Kacperski777 <49197589+Kacperski777@users.noreply.github.com>
2021-03-01 18:44:15 +0000

    Responsive Update (Smartphones)

--- login/login.php
+++ login/login.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 </head>
-
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 <link rel="stylesheet" href="css/form.css">
 
@@ -17,7 +17,7 @@
 
 
 
-<div class="form">
+<div class="form col-6">
 <h2> LOGIN </h2>
 <form action="includes/login.inc.php" method="post">
 <input class="input" type="text" name="uid" placeholder="Username/E-mail...">
--- login/signup.php
+++ login/signup.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 <link rel="stylesheet" href="css/form.css">
 
@@ -22,7 +22,7 @@
 
 
 
-<div class="form ">
+<div class="form col-6">
 <h2> SIGN UP </h2>
 
 <form action="includes/signup.inc.php" method="post">
